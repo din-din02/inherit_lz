470a3249a03ad114da6aad8fba9bbb2f7d6c68f8
din-din02 <din.tut02@gmail.com>
2026-03-30 20:35:15 +0300

    1

--- main1.py
+++ main1.py
@@ -4,7 +4,7 @@ def main():
     name = input("Введите имя собаки:")
     age = float(input("Введите возраст собаки:"))
     species = input("Введите вид собаки:")
-    breed = input("Введите название породы:")
+    breed = input("Введите название породы: ")
     guard_status = input("Введите статус охраны (да или нет):")
     dog = Dog(name,age,species,breed,guard_status)
     dog.info()
