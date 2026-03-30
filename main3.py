ce1b2aff54aed7e7eabbbdff39d1ae1ae3310917
din-din02 <din.tut02@gmail.com>
2026-03-30 20:37:46 +0300

    5

---
+++ main3.py
@@ -0,0 +1,16 @@
+from book import EBook
+
+def main():
+    title=input('Введите название книги:')
+    author=input('Введите автора книги:')
+    year=int(input('Введите год выпуска книги:'))
+    file_size=input('Введите размер книги:')
+    format=input('Введите формат книги(электронная или бумажная):')
+    free=input('Книгу можно получить бесплатно(да или нет)?')
+    a=EBook(title,author,year, file_size, format, free)
+    a.info()
+    a.actual()
+    a.is_available()
+    a.is_free()
+if __name__=='__main__':
+    main()
